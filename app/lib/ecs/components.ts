32cee6f0d1641c1877daf39ef068bcd09b0e889b
m-ku66 <johanmarcmiango15@gmail.com>
2025-03-13 12:30:02 -0400

    Added new types and more robust properties to ecs components

--- app/lib/ecs/components.ts
+++ app/lib/ecs/components.ts
@@ -1,4 +1,5 @@
 import { Component } from "./types";
+import { Affinity, Personality, StatusEffects } from "../game_types/types";
 
 // Player component - contains all player-specific attributes
 export interface PlayerComponent extends Component {
@@ -17,6 +18,13 @@ export interface MageComponent extends Component {
   maxMagia: number; // Maximum magical energy
   magiaRegenRate: number; // How much magia regenerates per turn
   agility: number; // Determines spell casting order
+  resistance: number; // Reduces incoming spell damage
+  wisdom: number; // Increases crit chance and spell uses
+  attackPower: number; // Base attack power
+  affinity: Affinity; // Elemental affinity
+  personality: Personality; // Personality trait
+  innateSpell: SpellComponent; // Innate spell
+  grimoire: SpellComponent[]; // List of spells the mage knows
   // Add more mage attributes here
 }
 
@@ -27,5 +35,8 @@ export interface SpellComponent extends Component {
   magiaCost: number; // Mana cost of the spell
   castingTime: number; // Time it takes to cast the spell
   basePower: number; // Base power of the spell
+  uses: number; // Number of times the spell can be cast
+  effects: StatusEffects[]; // List of effects the spell has
+  affinity: Affinity; // Elemental affinity of the spell
   // Add more spell attributes here
 }
---
+++ app/lib/game_types/types.ts
@@ -0,0 +1,14 @@
+export type Affinity = "fire" | "water" | "earth" | "wind" | "lightning";
+
+export type Personality =
+  | "extroverted"
+  | "introverted"
+  | "determined"
+  | "laid-back";
+
+export type StatusEffects =
+  | "burn"
+  | "slow"
+  | "paralysis"
+  | "poison"
+  | "petrify";
